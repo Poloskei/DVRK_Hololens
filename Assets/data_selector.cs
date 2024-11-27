758ea51fcf9303b33fa3edd08aa4c198359ea391
Palcsek Máté <palcsekmate@gmail.com>
2024-11-27 23:13:25 +0100

    DICOM
     - moved data_selector to FileList to simplify things
     - added DICOM support
     - made loading asynchonous
     - created AddComponent method to dress up VolumeRenderedObjects

--- Assets/data_selector.cs
+++ Assets/data_selector.cs
@@ -10,28 +10,25 @@ using UnityEngine.UI;
 using Unity.VisualScripting;
 using UnityVolumeRendering;
 using MixedReality.Toolkit.SpatialManipulation;
+using System.Threading.Tasks;
 
 
 
 public class DataSelector : MonoBehaviour
 {
-    [SerializeField] GameObject NearMenu;
-    [SerializeField] GameObject ListMenu;
+    //[SerializeField] GameObject ListMenu;
     [SerializeField] string PatientsFolder;
     [SerializeField] GameObject ActionButtonPrefab;
     [SerializeField] GameObject PatientBase;
     [SerializeField] GameObject PSM1Plane;
     // Start is called before the first frame update
 
-    void Awake()
+    void Start()
     {
         Debug.Log("Started");
         List<string> fileList = Directory.GetFiles(PatientsFolder).ToList();
-        foreach (var item in Directory.GetDirectories(PatientsFolder))
-        {
-            fileList.Add(item);
-        }
-
+        List<string> folderList = Directory.GetDirectories(PatientsFolder).ToList();
+        
         foreach (string file in fileList)
         {
             //get filenames
@@ -43,17 +40,31 @@ public class DataSelector : MonoBehaviour
                 //create button for each filename
                 GameObject dataButton = CreateButton(label);
 
-                dataButton.GetComponent<PressableButton>().OnClicked.AddListener(() => { PatientSelected(file); });
+                dataButton.GetComponent<PressableButton>().OnClicked.AddListener(async() => { await RawLoader(file); });
 
 
 
             }
-            else if (label.EndsWith(".dicom"))
+            else if (label.EndsWith(".nrrd"))
             {
-                GameObject dataButton = CreateButton(label);
+                //imagefile not implemented
+                //GameObject dataButton = CreateButton(label);
             }
         }
-        
+        Debug.Log(folderList.Count + " folders");
+        foreach (string folder in folderList) 
+        {
+            //string label = CutPatientFile(file);
+            List<string> filesInFolder = Directory.GetFiles(folder).ToList();
+            Debug.Log(filesInFolder.Count + " files in folder");
+            if (filesInFolder[0].EndsWith(".dcm"))
+            {
+                Debug.Log("Found DICOM folder");
+                string label = CutPatientFile(folder);
+                GameObject dataButton = CreateButton(label);
+                dataButton.GetComponent<PressableButton>().OnClicked.AddListener(async() => { await DICOMLoader(folder); });
+            }
+        }
     }
     
     
@@ -62,7 +73,7 @@ public class DataSelector : MonoBehaviour
     private GameObject CreateButton(string label)
     {
         GameObject dataButton = Instantiate(ActionButtonPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        dataButton.transform.SetParent(ListMenu.GetComponentInChildren<GridLayoutGroup>().gameObject.transform, false);
+        dataButton.transform.SetParent(this.GetComponentInChildren<GridLayoutGroup>().gameObject.transform, false);
         //set the buttons label to the name of the file
         foreach (var child in dataButton.GetComponentsInChildren<TextMeshProUGUI>(true))
         {
@@ -81,7 +92,7 @@ public class DataSelector : MonoBehaviour
     {
         return patient.Remove(0,PatientsFolder.Length+1);
     }
-    public void PatientSelected(string fileName)
+    public async Task RawLoader(string fileName)
     {
         Debug.Log("Loading " + fileName);
         
@@ -93,20 +104,40 @@ public class DataSelector : MonoBehaviour
                                           iniData.format,
                                           iniData.endianness,
                                           iniData.bytesToSkip);
-        VolumeDataset dataset = importer.Import();
+        VolumeDataset dataset = await importer.ImportAsync();
+        
         Debug.Log(dataset.name);
         
         VolumeRenderedObject vro = VolumeObjectFactory.CreateObject(dataset);
+        AddComponents(vro);
+    }
+
+    public async Task DICOMLoader(string fileName)
+    {
+        Debug.Log("Loading " + fileName);
+        List<string> filePaths = Directory.GetFiles(fileName).ToList();
+
+        IImageSequenceImporter importer = ImporterFactory.CreateImageSequenceImporter(ImageSequenceFormat.DICOM);
+        IEnumerable<IImageSequenceSeries> seriesList = await importer.LoadSeriesAsync(filePaths);
+
+        foreach (IImageSequenceSeries series in seriesList)
+        {
+            VolumeDataset dataset = await importer.ImportSeriesAsync(series);
+            
+            AddComponents(VolumeObjectFactory.CreateObject(dataset));
+        }
+    }
+
+    void AddComponents(VolumeRenderedObject vro)
+    {
         vro.AddComponent<BoxCollider>();
         vro.AddComponent<ObjectManipulator>();
         vro.AddComponent<TapToPlace>();
         vro.AddComponent<CrossSectionManager>();
         vro.GetComponent<Transform>().transform.position = PatientBase.transform.position;
-        
-        vro.transform.SetParent(PatientBase.transform,true);
+
+        vro.transform.SetParent(PatientBase.transform, true);
 
         PSM1Plane.GetComponent<CrossSectionPlane>().SetTargetObject(vro);
-
-        //vro.SetParent(PatientBase.transform, false);
     }
 }
